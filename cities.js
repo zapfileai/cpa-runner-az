2070061f103ef560bd1fb3b26161b7b7a3ce9431
Sreehari Annam <sannam@wescodist.com>
2026-06-30 07:19:01 -0400

    feat: load first 80 cities from cities.csv

--- cities.js
+++ cities.js
@@ -1,5 +1,20 @@
-﻿const arizonaCities = [
-  // paste first 80 cities from cities.csv here
+const arizonaCities = [
+  "Alpine", "Apache Junction", "Arivaca", "Arizona City", "Avondale",
+  "Bellemont", "Benson", "Bisbee", "Buckeye", "Bullhead City",
+  "Bylas", "Camp Verde", "Carefree", "Casa Grande", "Catalina",
+  "Cave Creek", "Chandler", "Chino Valley", "Clarkdale", "Clifton",
+  "Cochise", "Congress", "Coolidge", "Cornville", "Cottonwood",
+  "Dewey", "Douglas", "Eagar", "El Mirage", "Eloy",
+  "Flagstaff", "Florence", "Fort Huachuca", "Fort Mohave", "Fountain Hills",
+  "Gila Bend", "Gilbert", "Glendale", "Globe", "Gold Canyon",
+  "Goodyear", "Grand Canyon", "Green Valley", "Hayden", "Heber",
+  "Hereford", "Higley", "Holbrook", "Jerome", "Joseph City",
+  "Kingman", "Lake Havasu City", "Lakeside", "Laveen", "Litchfield Park",
+  "Lukeville", "Marana", "Maricopa", "Meadview", "Mesa",
+  "Miami", "Morenci", "Morristown", "Munds Park", "New River",
+  "Nogales", "Oracle", "Overgaard", "Page", "Paradise Valley",
+  "Parker", "Parks", "Patagonia", "Paulden", "Payson",
+  "Peach Springs", "Peoria", "Phoenix", "Picacho", "Pima",
 ];
 
 const BATCH_SIZE = 5;
